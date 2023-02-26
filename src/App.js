ecdaa5671da29a5ad45140ff90b549b04707e934
lomeat <lom3at@gmail.com>
2023-02-27 01:46:37 +0400

    player sprite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Stage, Container, Sprite } from "@pixi/react";
+import { Ticker } from "pixi.js";
+
+import player from "./assets/player.png";
 
 export function App() {
   const [width, height] = [500, 500];
@@ -12,7 +15,7 @@ export function App() {
         height,
       }}
     >
-      <Scene>
+      <Scene sizes={[width, height]}>
         <Player />
       </Scene>
     </Stage>
@@ -30,5 +33,40 @@ export function Scene(props) {
 }
 
 export function Player() {
-  return <Sprite x={100} y={100}></Sprite>;
+  const [speed, setSpeed] = React.useState(0);
+
+  function move(dt) {}
+
+  Ticker.shared.add(move);
+
+  return (
+    <Sprite x={100} y={100} image={player} width={50} height={50}></Sprite>
+  );
 }
+
+// import { BlurFilter } from "pixi.js";
+// import { Stage, Container, Sprite, Text } from "@pixi/react";
+// import { useMemo } from "react";
+
+// export const App = () => {
+//   const blurFilter = useMemo(() => new BlurFilter(4), []);
+
+//   return (
+//     <Stage>
+//       <Sprite
+//         image="https://pixijs.io/pixi-react/img/bunny.png"
+//         x={400}
+//         y={270}
+//         anchor={{ x: 0.5, y: 0.5 }}
+//       />
+
+//       <Container x={400} y={330}>
+//         <Text
+//           text="Hello World"
+//           anchor={{ x: 0.5, y: 0.5 }}
+//           filters={[blurFilter]}
+//         />
+//       </Container>
+//     </Stage>
+//   );
+// };
