7ca870eafc90d81dfa6611a28ca29343661594c7
lomeat <lom3at@gmail.com>
2023-02-27 01:19:41 +0400

    react-pixi got old

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { Stage, Container, Sprite } from "@pixi/react";
 
 export function App() {
-  return <div>Hello there</div>;
+  const [width, height] = [500, 500];
+
+  return (
+    <Stage
+      options={{
+        backgroundColor: 0xffffff,
+        width,
+        height,
+      }}
+    >
+      <Scene>
+        <Player />
+      </Scene>
+    </Stage>
+  );
+}
+
+export function Scene(props) {
+  const [width, height] = props?.sizes;
+
+  return (
+    <Container width={width} height={height}>
+      {props.children}
+    </Container>
+  );
+}
+
+export function Player() {
+  return <Sprite x={100} y={100}></Sprite>;
 }
