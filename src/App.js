7b29f21dddbc987f5823039d0edb049abba6349e
lomeat <lom3at@gmail.com>
2023-02-27 07:12:17 +0400

    move player error

--- src/App.js
+++ src/App.js
@@ -1,72 +1,73 @@
 import React from "react";
-import { Stage, Container, Sprite } from "@pixi/react";
-import { Ticker } from "pixi.js";
+import {
+  Stage,
+  Container,
+  Sprite,
+  useTick,
+  AppProvider,
+  useApp,
+} from "@pixi/react";
 
 import player from "./assets/player.png";
 
 export function App() {
   const [width, height] = [500, 500];
 
+  const stageProps = {
+    width,
+    height,
+    options: {
+      backgroundColor: 0xffffff,
+      antialisis: true,
+    },
+  };
+
+  const containerStyle = {
+    display: "block",
+    border: "2px solid black",
+    width,
+    height,
+    margin: "100px auto",
+  };
+
+  // const { move, x } = PlayerController();
+
+  // move();
+
   return (
-    <Stage
-      options={{
-        backgroundColor: 0xffffff,
-        width,
-        height,
-      }}
-    >
-      <Scene sizes={[width, height]}>
-        <Player />
-      </Scene>
-    </Stage>
+    <div style={{ ...containerStyle }}>
+      <Stage {...stageProps}>
+        <Scene sizes={[width, height]}>
+          <Player />
+        </Scene>
+      </Stage>
+    </div>
   );
 }
 
 export function Scene(props) {
   const [width, height] = props?.sizes;
 
+  const app = useApp();
+
   return (
     <Container width={width} height={height}>
-      {props.children}
+      <AppProvider value={app}>{props.children}</AppProvider>
     </Container>
   );
 }
 
-export function Player() {
-  const [speed, setSpeed] = React.useState(0);
+function Player(props) {
+  // const [speed, setSpeed] = React.useState(10);
+  const speed = 10;
 
-  function move(dt) {}
+  const [x, setX] = React.useState(0);
 
-  Ticker.shared.add(move);
+  function move(dt) {
+    setX((state) => state * dt * speed);
+  }
 
-  return (
-    <Sprite x={100} y={100} image={player} width={50} height={50}></Sprite>
-  );
+  useTick((dt) => move(dt));
+
+  return <Sprite y={100} x={x} image={player} width={50} height={50}></Sprite>;
 }
-
-// import { BlurFilter } from "pixi.js";
-// import { Stage, Container, Sprite, Text } from "@pixi/react";
-// import { useMemo } from "react";
-
-// export const App = () => {
-//   const blurFilter = useMemo(() => new BlurFilter(4), []);
-
-//   return (
-//     <Stage>
-//       <Sprite
-//         image="https://pixijs.io/pixi-react/img/bunny.png"
-//         x={400}
-//         y={270}
-//         anchor={{ x: 0.5, y: 0.5 }}
-//       />
-
-//       <Container x={400} y={330}>
-//         <Text
-//           text="Hello World"
-//           anchor={{ x: 0.5, y: 0.5 }}
-//           filters={[blurFilter]}
-//         />
-//       </Container>
-//     </Stage>
-//   );
-// };
