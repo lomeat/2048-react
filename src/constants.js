202d3941e7d2ef81c28ce0f8240148398137a129
lomeat <lom3at@gmail.com>
2023-03-10 10:37:15 +0400

    colors

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const initBoard = [
   [0, 0, 0, 0],
-  [0, 0, 0, 4],
-  [2, 0, 2, 0],
-  [8, 4, 2, 2],
+  [0, 0, 0, 2],
+  [0, 0, 0, 0],
+  [0, 2, 0, 0],
 ];
 export const boardWidth = initBoard.length;
 export const boardHeight = initBoard[0].length;
@@ -13,13 +13,13 @@ export const cellHeight = 100;
 export const colors = {
   2: "#f1d5c5",
   4: "#b1c3b2",
-  8: "black",
-  16: "black",
-  32: "black",
-  64: "black",
-  128: "black",
-  256: "black",
-  512: "black",
-  1024: "black",
-  2048: "black",
+  8: "#77986f",
+  16: "#d4a772",
+  32: "#3d534b",
+  64: "#6c7355",
+  128: "#3b4834",
+  256: "#FF9918",
+  512: "#F26B02",
+  1024: "#B41004",
+  2048: "#2e2c20",
 };
